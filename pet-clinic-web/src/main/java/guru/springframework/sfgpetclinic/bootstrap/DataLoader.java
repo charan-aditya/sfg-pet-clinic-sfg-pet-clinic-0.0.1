578a0ae8a9aad7a4f1f01d2b81adfbbcccae8804
x03757 <adithya.padakandla@delta.com>
2022-06-05 19:41:50 -0400

    data loader

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class DataLoader implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
