0f857e1a4ff754e8f856a3fd48eae2ed3ddf6676
x03757 <adithya.padakandla@delta.com>
2022-06-10 21:08:53 -0400

    added visit speciality,owner and vet

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -8,6 +8,9 @@ import java.util.Set;
 public class Owner extends Person {
 
     public Set<Pet> pets;
+    private String city;
+    private String telephone;
+    private String address;
 
     public Set<Pet> getPets() {
         return pets;
@@ -15,5 +18,29 @@ public class Owner extends Person {
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
